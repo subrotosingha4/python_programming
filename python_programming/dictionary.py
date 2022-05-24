fe75229e6395aa87f624b9a4e3c97bc99031a456
Subroto Singha <subroto.singha4@gmail.com>
2022-05-23 20:55:12 -0500

    pythonically access dictionary

---
+++ python_programming/dictionary.py
@@ -0,0 +1,4 @@
+ls_dict={'name1':'subroto','name2':'priyangka','name3':'singha','name4':'roy'}
+#use tuple unpacking to print out key and value from dictionary
+for key,name in ls_dict.items():
+    print(key,name)
