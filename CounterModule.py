215fbfc4265a7d6dc865507c6bf14254cc3a624c
Subroto Singha <subroto.singha4@gmail.com>
2022-05-24 20:56:48 -0500

    counting in python

---
+++ CounterModule.py
@@ -0,0 +1,8 @@
+#as a data scientist you often need to count items
+#frequency analytics
+#counter module is a go to library since the behind concept is based on dictionary
+#so, easy to count as key,value
+from collections import Counter
+ls_dict=['stationname', 'Austin-Forest Park', 'Austin-Forest Park', 'Austin-Forest Park', 'Austin-Forest Park', 'Austin-Forest Park', 'Austin-Forest Park', 'Austin-Forest Park', 'Austin-Forest Park', 'Austin-Forest Park', 'Austin-Forest Park', 'Austin-Forest Park', 'Austin-Forest Park', 'Austin-Forest Park', 'Austin-Forest Park', 'Austin-Forest Park', 'Austin-Forest Park', 'Austin-Forest Park', 'Austin-Forest Park', 'Austin-Forest Park', 'Austin-Forest Park']
+name_count=Counter(ls_dict)
+print(name_count)
